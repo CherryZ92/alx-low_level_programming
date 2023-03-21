6f72f606c76f3f4665c7c6742ff91fc1ec24cebe
CherryZ92 <charifa.zakar@gmail.com>
2023-03-21 13:16:36 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
   */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90 || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
@@ -14,4 +14,5 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
+	_putchar('\n');
 }
