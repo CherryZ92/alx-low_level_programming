c3562fb5c35c0553f6c668bd5dc33f41fbb738b9
CherryZ92 <charifa.zakar@gmail.com>
2023-03-23 09:46:46 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,16 +2,13 @@
 /**
   *_isupper: returns 1 if c is uppercase 0 oherwise
   *@c: a characte in ASCII code
-  *Return; 0 upper 1 otherwise
+  *Return: 0 upper 1 otherwise
   */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (0);
-	}
-	else
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
+	else
+		return (0);
+
 }
